6d60b9d2f9c3af6da889aa9626b15b7d80858ff9
Wilker Lucio <wilkerlucio@gmail.com>
2017-10-22 21:27:51 -0200

    Clean changelog and update description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.wsscode.oge "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Om Graph Explorer"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
