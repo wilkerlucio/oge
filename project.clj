eea0215c86437cdd2a0b4475ec20bf458a9eac96
Wilker Lucio <wilkerlucio@gmail.com>
2018-01-23 21:53:22 -0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,15 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [org.clojure/core.async "0.3.443"]
+                 [org.clojure/core.async "0.3.465"]
 
                  [spec-coerce "1.0.0-alpha5"]
-                 [org.omcljs/om "1.0.0-beta1"]
-                 [fulcrologic/fulcro "1.0.0"]
-                 [fulcrologic/fulcro-css "1.0.0"]
-                 [com.wsscode/pathom "1.0.0-beta9-SNAPSHOT"]
+                 [fulcrologic/fulcro "2.1.2"]
+                 [fulcrologic/fulcro-css "2.0.0"]
+                 [com.wsscode/pathom "2.0.0-beta1"]
 
                  [cljsjs/codemirror "5.24.0-1"]
                  [cljsjs/d3 "4.3.0-5"]
